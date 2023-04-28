c1f8a32e29aac4ac04e3196248025e5fdb7bd231
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-04-28 20:46:00 -0300

    exercicio em sala completo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿double carne, acompanhamentos, refrigerante, cerveja, agua;
+int adultos0alcool, adultosalcool, adultos, convidados, crianca;
+
+Console.WriteLine ("==Churras==");
+
+Console.Write("Adultos que não ingerem Alcool..........");
+adultos0alcool = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Adultos que ingerem Alcool..............");
+adultosalcool = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Crianças.................................");
+crianca = Convert.ToInt32(Console.ReadLine()!);
+
+adultos = adultos0alcool + adultosalcool;
+convidados = adultos + crianca;
+
+carne = 0.4 * adultos + 0.2 * crianca;
+acompanhamentos = 0.2 * convidados;
+cerveja = 2 * adultosalcool;
+refrigerante = 0.5 * adultos0alcool + crianca;
+agua = 0.4 * convidados;
+
+Console.WriteLine($"Carne.............:{carne:N1}Kg");
+Console.WriteLine($"Acompanhamentos...:{acompanhamentos:N1}Kg");
+Console.WriteLine($"Cerveja...........:{cerveja:N1}L");
+Console.WriteLine($"Refrigerante......:{refrigerante:N1}L");
+Console.WriteLine($"agua..............:{agua:N1}L");
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Churrasco
+Faça um programa que calcule a quantidade de itens necessários para um churrasco a partir da quantidade de adultos (que consomem e que não consomem bebidas alcoólicas) e de crianças.
+
+Item	     Por adulto	   Por criança
+Carne	        400g	      200g
+Acompanhamentos	200g	      200g
+Cerveja	         2l	           --
+Refrigerante	500ml	      500ml
+Água	        400ml	      400ml
+Estime cerveja e refrigerantes de acordo com a indicação de consumo dos adultos. Estime refrigerante para todas as crianças. Estime água para todos.
+
+Exemplo:
+
+--- Churrasco ---
+
+Adultos (que consomem bebidas alcoólicas).......: 5
+Adultos (que não consomem bebidas alcoólicas)...: 3
+Crianças........................................: 6
+
+Carne.............: 4,4kg
+Acompanhamentos...: 2,8kg
+Cerveja...........: 10,0l
+Refrigerante......: 4,5l
+Água..............: 5,6l
